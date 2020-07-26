b6abebb3571ebad31db8f353d74f86d5b7f551ac
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-07-26 11:11:51 +0430

    f3

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+
+Vue.component('todo-item',{
+  props:['todo'],
+  template:'<li>{{todo.text}}</li>'
+})
+
+
+
+
+
+
 var app= new Vue({
-  el:"#app-2",
+  el:"#app-3",
   data:{
-    message:"you loaded this page on " + new Date().toLocaleString() +" :)"
+    grocerylist:[
+      {id:0,text:'vegtables'},
+      {id:1,text:'cheese'},
+      {id:2,text: 'what ever else humans are supposed to eat'}
+    ]
   }
 })
